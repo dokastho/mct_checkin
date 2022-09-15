c30a79e6cf9a2c2c2c66720e834f39e4f5949d79
Thomas Dokas <dokastho@umich.edu>
2022-09-15 18:11:48 +0000

    mail sending fault tolerance

--- mct_checkin/gmail.py
+++ mct_checkin/gmail.py
@@ -4,22 +4,26 @@ import datetime
 
 def send_mail(attendance: list):
 
-    # creates SMTP session
-    s = smtplib.SMTP('smtp.gmail.com', 587)
+    try:
+        # creates SMTP session
+        s = smtplib.SMTP('smtp.gmail.com', 587)
 
-    # start TLS for security
-    s.starttls()
+        # start TLS for security
+        s.starttls()
 
-    # Authentication
-    s.login("mctattendance", "michigan_cycling")
+        # Authentication
+        s.login("mctattendance", "michigan_cycling")
 
-    # message to be sent
-    sub = f'Group Ride Attendance {datetime.date.today()}'
-    txt = '\n'.join(attendance)
-    message = 'Subject: {}\n\n{}'.format(sub, txt)
+        # message to be sent
+        sub = f'Group Ride Attendance {datetime.date.today()}'
+        txt = '\n'.join(attendance)
+        message = 'Subject: {}\n\n{}'.format(sub, txt)
 
-    # sending the mail
-    s.sendmail("mctattendance@gmail.com", "cycling.officers@gmail.com", message)
+        # sending the mail
+        s.sendmail("mctattendance@gmail.com", "cycling.officers@gmail.com", message)
 
-    # terminating the session
-    s.quit()
+        # terminating the session
+        s.quit()
+        
+    except:
+        print("unable to send mail")
